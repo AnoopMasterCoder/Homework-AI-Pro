2f8b4afc80a142d56b32099b4e1cd923e5b176c1
AnoopMasterCoder <anoop.github@gmail.com>
2022-12-31 16:56:54 +0530

    Updated the server URL and removed package-lock.json files

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
 	// Fetch data from server -> Bot's response
 
-	const response = await fetch('http://localhost:5000', {
+	const response = await fetch('https://homework-ai-pro.onrender.com', {
 		method: 'POST',
         headers: {
             'Content-Type': 'application/json',
